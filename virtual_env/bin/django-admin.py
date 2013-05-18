41f298caaa779ad645239d519e8bbe60d95efa49
josh <giggaflop@gmail.com>
2013-05-18 15:22:25 +0100

    Remove VirtualEnv

--- virtual_env/bin/django-admin.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2.7
-
-import os; activate_this=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'activate_this.py'); execfile(activate_this, dict(__file__=activate_this)); del os, activate_this
-
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
